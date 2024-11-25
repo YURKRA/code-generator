c5fce688700a895fb81e2cceed52fb8fe7e4eae0
tuxuan <fjxpen1234@gmail.com>
2024-11-26 02:40:28 +0800

    add:PicoCli命令行工具

--- code-generator-basic/src/main/java/com/code/generator/MainGenerator.java
+++ code-generator-basic/src/main/java/com/code/generator/MainGenerator.java
@@ -18,14 +18,14 @@ public class MainGenerator {
     public static void doGenerate(Object model) throws TemplateException, IOException {
         String projectPath = System.getProperty("user.dir");
         // 整个项目的根路径
-        File parentFile = new File(projectPath);
+        File parentFile = new File(projectPath).getParentFile();
         // 输入路径
         String inputPath = new File(parentFile, "code-generator-demo-projects/acm-template").getAbsolutePath();
         String outputPath = projectPath;
         // 生成静态文件
         StaticGenerator.copyFilesByRecursive(inputPath, outputPath);
         // 生成动态文件
-        String inputDynamicFilePath = projectPath + File.separator + "code-generator-basic" + File.separator + "src/main/resources/templates/MainTemplate.java.ftl";
+        String inputDynamicFilePath = projectPath + File.separator + "src/main/resources/templates/MainTemplate.java.ftl";
         String outputDynamicFilePath = outputPath + File.separator + "acm-template/src/com/yupi/acm/MainTemplate.java";
         DynamicGenerator.doGenerate(inputDynamicFilePath, outputDynamicFilePath, model);
     }

d06f5697a6092d6de170c7f61850f6ad01ff6d66
tuxuan <fjxpen1234@gmail.com>
2024-11-26 02:07:44 +0800

    add:命令模式示例

---
+++ code-generator-basic/src/main/java/com/code/cli/pattern/Command.java
@@ -0,0 +1,5 @@
+package com.code.cli.pattern;
+
+public interface Command {
+    void execute();
+}
---
+++ code-generator-basic/src/main/java/com/code/cli/pattern/RemoteControl.java
@@ -0,0 +1,14 @@
+package com.code.cli.pattern;
+
+public class RemoteControl {
+    private Command command;
+
+    public void setCommand(Command command) {
+        this.command = command;
+    }
+
+    public void pressButton() {
+        command.execute();
+    }
+}
+
